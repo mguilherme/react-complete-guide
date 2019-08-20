9fa2e1e29f612133e27639a001c680dcf5381de6
“Miguel <mguilherme82@gmail.com>
2019-08-20 09:44:41 +0100

    53. Handling Dynamic Content
    The JavaScript Way53. Handling Dynamic Content
    
    The JavaScript Way53. Handling Dynamic Content
    
    The JavaScript Way53. Handling Dynamic Content
    
    The JavaScript Way53. Handling Dynamic Content
    
    The JavaScript Way53. Handling Dynamic Content
    
    The JavaScript Way53. Handling Dynamic Content
    
    The JavaScript Way53. Handling Dynamic Content
    
    The JavaScript Way53. Handling Dynamic Content
    
    The JavaScript Way53. Handling Dynamic Content
    
    The JavaScript Way53. Handling Dynamic Content
    
    The JavaScript Way53. Handling Dynamic Content
    
    The JavaScript Way53. Handling Dynamic Content
    
    The JavaScript Way53. Handling Dynamic Content
    
    The JavaScript Way53. Handling Dynamic Content
    
    The JavaScript Way53. Handling Dynamic Content
    
    The JavaScript Way53. Handling Dynamic Content
    
    The JavaScript Way53. Handling Dynamic Content
    
    The JavaScript Way53. Handling Dynamic Content
    
    The JavaScript Way53. Handling Dynamic Content
    
    The JavaScript Way53. Handling Dynamic Content
    
    The JavaScript Way53. Handling Dynamic Content
    
    The JavaScript Way53. Handling Dynamic Content
    
    The JavaScript Way53. Handling Dynamic Content
    
    The JavaScript Way53. Handling Dynamic Content
    
    The JavaScript Way53. Handling Dynamic Content
    
    The JavaScript Way53. Handling Dynamic Content
    
    The JavaScript Way53. Handling Dynamic Content
    
    The JavaScript Way53. Handling Dynamic Content
    
    The JavaScript Way53. Handling Dynamic Content
    
    The JavaScript Way53. Handling Dynamic Content
    
    The JavaScript Way53. Handling Dynamic Content
    
    The JavaScript Way53. Handling Dynamic Content
    
    The JavaScript Way53. Handling Dynamic Content
    
    The JavaScript Way53. Handling Dynamic Content
    
    The JavaScript Way53. Handling Dynamic Content
    
    The JavaScript Way53. Handling Dynamic Content
    
    The JavaScript Way53. Handling Dynamic Content
    
    The JavaScript Way53. Handling Dynamic Content
    
    The JavaScript Way53. Handling Dynamic Content
    
    The JavaScript Way53. Handling Dynamic Content
    
    The JavaScript Way53. Handling Dynamic Content
    
    The JavaScript Way53. Handling Dynamic Content
    
    The JavaScript Way53. Handling Dynamic Content
    
    The JavaScript Way53. Handling Dynamic Content
    
    The JavaScript Way53. Handling Dynamic Content
    
    The JavaScript Way53. Handling Dynamic Content
    
    The JavaScript Way53. Handling Dynamic Content
    
    The JavaScript Way53. Handling Dynamic Content
    
    The JavaScript Way53. Handling Dynamic Content
    
    The JavaScript Way53. Handling Dynamic Content
    
    The JavaScript Way53. Handling Dynamic Content
    
    The JavaScript Way53. Handling Dynamic Content
    
    The JavaScript Way

--- src/App.js
+++ src/App.js
@@ -51,6 +51,31 @@ class App extends Component {
             cursor: 'pointer'
         };
 
+        let persons = null;
+
+        if (this.state.showPersons) {
+            persons = (
+                <div>
+                    <Person
+                        name={this.state.persons[0].name}
+                        age={this.state.persons[0].age}
+                    />
+                    <Person
+                        name={this.state.persons[1].name}
+                        age={this.state.persons[1].age}
+                        click={this.switchNameHandler.bind(this, 'Max')}
+                        changed={this.nameChangedHandler}
+                    >
+                        My Hobbies: Racing
+                    </Person>
+                    <Person
+                        name={this.state.persons[2].name}
+                        age={this.state.persons[2].age}
+                    />
+                </div>
+            );
+        }
+
         return (
             <div className="App">
                 <h1>Hi, I'm a React App</h1>
@@ -59,27 +84,9 @@ class App extends Component {
                     style={style}
                     onClick={this.togglePersonsHandler}>Switch Name
                 </button>
-                {
-                    this.state.showPersons ?
-                        <div>
-                            <Person
-                                name={this.state.persons[0].name}
-                                age={this.state.persons[0].age}
-                            />
-                            <Person
-                                name={this.state.persons[1].name}
-                                age={this.state.persons[1].age}
-                                click={this.switchNameHandler.bind(this, 'Max')}
-                                changed={this.nameChangedHandler}
-                            >
-                                My Hobbies: Racing
-                            </Person>
-                            <Person
-                                name={this.state.persons[2].name}
-                                age={this.state.persons[2].age}
-                            />
-                        </div> : null
-                }
+
+                {persons}
+
             </div>
         );
         // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does this work now?'));
